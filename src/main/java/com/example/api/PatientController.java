3d177fa3cd144e689ade756ab7680231587c8e31
LAPTOP-JLKE1UH2\Marcin <ubysz.marcin@gmail.com>
2022-11-14 22:56:06 +0100

    get from /id

--- src/main/java/com/example/api/PatientController.java
+++ src/main/java/com/example/api/PatientController.java
@@ -5,13 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 public class PatientController {
+    public int id=8;
     @Autowired
     JdbcTemplate jdbcTemplate;
     @GetMapping(path = "/patients")
@@ -19,4 +21,39 @@ public class PatientController {
         List<Patient> patients = jdbcTemplate.query("SELECT * FROM patient",new BeanPropertyRowMapper(Patient.class));
         return new ResponseEntity<>(patients, HttpStatus.OK);
     }
+    @PostMapping(path = "/patients",consumes = "application/json")
+    public String addPatient(@RequestBody Patient newPatient)
+    {
+        String insert_query = "INSERT INTO patient (id, patient_name,patient_age,patient_lastname) VALUE(?,?,?,?);";
+        jdbcTemplate.update(insert_query,id,newPatient.getPatient_name(),newPatient.getPatient_age(),newPatient.getPatient_lastname());
+        id++;
+    return "dodano nowego pacjenta "+newPatient.getPatient_name();
+    }
+
+    @GetMapping(path = "/patients/")
+    @ResponseBody
+    public String getPatient(@RequestParam int newId){
+        //String insert_query ="SELECT patient_name FROM patient WHERE id=noweId";
+         String name ="ID "+newId;
+        //String name = jdbcTemplate.queryForObject(insert_query,String.class);
+        return name;
+    }
+
+    @RequestMapping(value = "/patients/{noweid}", method = GET)
+    @ResponseBody
+    public String getFoosBySimplePathWithPathVariable(@PathVariable("noweid")int noweid) {
+System.out.println(noweid);
+        String insert_query ="SELECT patient_name FROM patient WHERE id=noweid;";
+        jdbcTemplate.update(insert_query,noweid);
+
+
+
+       // String name ="ID "+newId;
+        String name = jdbcTemplate.queryForObject(insert_query,String.class);
+        System.out.println(name);
+        return name;
+      //  return "Get a specific Foo with id=" + id;
+    }
+
+
 }
